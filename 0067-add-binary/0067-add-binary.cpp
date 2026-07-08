32d2d5ec06c254db53692a0df5f58906e13f8bd9
Ernazar Nygymet <ynigimet4@outlook.com>
2026-07-08 11:21:45 +0200

    0067-ADD-BINARY Stats: Time: 0 ms (100%), Space: 8.8 MB (97.15%) - LeetHub

---
+++ 0067-add-binary/0067-add-binary.cpp
@@ -0,0 +1,93 @@
+class Solution {
+public:
+    string addBinary(string a, string b) {
+        int sum, total, carry=0;
+        string out;
+        if(a.length() > b.length()) {
+            out = a;
+        }
+        else {out = b;}
+        int count = out.length()-1;
+        // Input:    "1010"
+        //                  "1011"
+        // Output:   "1101"
+        // Expected: "10101"
+        if(a.length() == 1 && b.length() == 1) {
+            if(a.at(0) > b.at(0)){
+                out = a;
+            } else {out = b;}
+        }
+        int j = b.length() - 1; // b = last character
+        int real_a, real_b;
+
+        for(int i = a.length() - 1; count>=0; count--) {
+            if(i < 0) {
+                real_a = 0;   
+            } else { real_a = a.at(i) - '0'; }
+            if(j < 0) {
+                real_b = 0;
+            } else { real_b = b.at(j) - '0'; }
+            
+            sum = real_b + real_a + carry;        
+            carry = 0;
+            
+            if(count == 0) {
+                if(sum == 2 || sum == 3) {    
+                    total = sum % 2; // 2 % 2 = 0 && 3 % 2 = 1
+                    carry = sum / 2; // 2 / 2 = 1 && 3 / 2 = 1
+                    if(carry == 1 && total == 1) {
+                        total = total + '0';
+                        char result = total;
+                        out.at(count) = result;
+                        out.insert(out.begin(), '1');
+                        return out;
+                    }
+                    if(carry == 1 || total == 0) {
+                        total = carry;
+                    }
+                    total = total + '0';
+                    char result = total;
+                    out.at(count) = result;
+                    if(out.compare(count+1, 1, "0") == 0) {
+                        out.insert(out.begin()+1, '0'); 
+                    }
+                    if(out.compare(count+1, 1, "1") == 0) {
+                        out.insert(out.begin()+1, '0'); 
+                    }
+                    if(out.length() == 1) {
+                        out.insert(out.begin()+1, '0');
+                    }
+                    return out;
+                }
+                if(sum == 1 && out == "0" ||  sum == 1 && out == "1") {
+                    return out;
+                }
+                
+                if(sum == 0 && out == "0") {
+                    
+                    return out;
+                }
+            }
+            
+            if(sum == 2 || sum == 3) {
+                total = sum % 2; // 2 % 2 = 0 && 3 % 2 = 1
+                carry = sum / 2; // 2 / 2 = 1 && 3 / 2 = 1
+            }
+            if(sum == 1 || sum == 0) {
+                total = sum; 
+            }
+            // converting vice versa to char.
+        
+            total = total + '0';
+            char result = total;
+            out.at(count) = result;               
+            j--;
+            i--;
+        }
+        return out;
+    }
+};
+
+
+
+
--- README.md
+++ README.md
@@ -13,9 +13,19 @@
 |  |
 | ------- |
 | [0009-palindrome-number](https://github.com/Ful41k/code-workout/tree/master/0009-palindrome-number) |
+| [0067-add-binary](https://github.com/Ful41k/code-workout/tree/master/0067-add-binary) |
 | [4135-concatenate-non-zero-digits-and-multiply-by-sum-i](https://github.com/Ful41k/code-workout/tree/master/4135-concatenate-non-zero-digits-and-multiply-by-sum-i) |
 ## String
 |  |
 | ------- |
 | [0058-length-of-last-word](https://github.com/Ful41k/code-workout/tree/master/0058-length-of-last-word) |
+| [0067-add-binary](https://github.com/Ful41k/code-workout/tree/master/0067-add-binary) |
+## Bit Manipulation
+|  |
+| ------- |
+| [0067-add-binary](https://github.com/Ful41k/code-workout/tree/master/0067-add-binary) |
+## Simulation
+|  |
+| ------- |
+| [0067-add-binary](https://github.com/Ful41k/code-workout/tree/master/0067-add-binary) |
 <!---LeetCode Topics End-->
