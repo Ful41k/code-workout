39c8c4dc9db409f90d915c09ff83b4fa3e874277
Ernazar Nygymet <ynigimet4@outlook.com>
2026-07-08 13:42:12 +0200

    2058-CONCATENATION-OF-ARRAY Stats: Time: 3 ms (10.29%), Space: 16.9 MB (41.47%) - LeetHub

--- 2058-concatenation-of-array/2058-concatenation-of-array.cpp
+++ 2058-concatenation-of-array/2058-concatenation-of-array.cpp
@@ -2,10 +2,8 @@ class Solution {
 public:
     vector<int> getConcatenation(vector<int>& nums) {
         vector<int> ans = nums;
-        int m=0;
-        int common = ans.size() * 2;
-        ans.resize(common); // 6
-        for(int k=0; k < ans.size(); k++) {
+        ans.resize(ans.size() * 2); // 6
+        for(int k=0, m=0; k < ans.size(); k++) {
             if(m == nums.size()) {
                 m = 0;
             }
