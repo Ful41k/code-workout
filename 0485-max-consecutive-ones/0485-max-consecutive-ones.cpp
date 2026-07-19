d7d16ebb2443ebafee9e457d6ac6b0e63aef341a
Ernazar Nygymet <ynigimet4@outlook.com>
2026-07-19 15:26:53 +0200

    0485-MAX-CONSECUTIVE-ONES Stats: Time: 11 ms (2.89%), Space: 50.1 MB (93.78%) - LeetHub

--- 0485-max-consecutive-ones/0485-max-consecutive-ones.cpp
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.cpp
@@ -1,35 +1,25 @@
 class Solution {
 public:
     int findMaxConsecutiveOnes(vector<int>& nums) {
-        // make another case: 
-        // where firstly I will find all 1
-        // then I will find each index
-        // and if the indexes are moving consecuitive then I will return it
-        vector<int> goat;
-        int consec=1, max=0;
-        for(int k=0; k < nums.size(); k++) {
-            if(nums.at(k) == 1) {
-                goat.push_back(k);
-            }
-        }// I constructed a new vector with their indexes of 1
-        // [0,2,3,5]
-        // [0,1,3,4,5]
-        if(goat.size() == 0) {
-            return 0;
+        int max=0, current=0, count = 1;
+        if(nums.size() == 1) {
+            if(nums.at(0) == 1) return 1;
+            else  return 0;
         }
-        if(goat.size() == 1) {
-            return 1;
+        
+        for(int i=0, j=1; i < nums.size()-1; i++, j++) {
+            if(nums.at(i) == nums.at(j) && nums.at(i) == 1 && nums.at(j) == 1) {
+                count++;
+                current = count;
+            }
+            if(nums.at(i) != nums.at(j)) {
+                count = 1;
+                current = count;
+            }
+            if(max < current) {
+                max = current;
+            }
         }
-        for(int u=0; u < goat.size()-1;u++) {
-            if(goat.at(u)+1 == goat.at(u+1)) {
-                consec++;
-            } else {
-                consec = 1;
-            }
-            if(max < consec) {
-                max = consec;
-            }
-        } // consec = 3
         return max;
     }
 };
